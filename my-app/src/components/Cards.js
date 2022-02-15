72f50923b7ec8ca4bfc27a6e5372be8ecc4e4854
Alexander Obregon <alexander.obregon47@gmail.com>
2022-02-15 15:44:36 -0800

    added persistent likes

--- my-app/src/components/Cards.js
+++ my-app/src/components/Cards.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Card,
@@ -11,6 +11,8 @@ import {
 } from "react-bootstrap";
 
 function Cards({ artist, photo, location, date, likes, posts, setPosts, post }) {
+
+  const [postLikes, setPostLikes] = useState()
 
   function handleDeleteClick() {
     console.log("test")
@@ -31,6 +33,32 @@ function Cards({ artist, photo, location, date, likes, posts, setPosts, post }) 
   
   }
 
+
+  function handleLikes() {
+    
+    setPostLikes(postLikes + 1)
+
+    fetch(`http://localhost:9292/post/${post.id}`, {
+      method: 'PATCH',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({likes: post.likes + 1})
+    })
+  
+  .then((res) => res.json())
+  .then((updatedItem) => handleUpdateLikes(updatedItem))
+  }
+
+  function handleUpdateLikes(updatedItem) {
+    const updatedItems = posts.map((post) => {
+      if(post.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return post;
+      }
+    });
+    setPosts(updatedItems);
+    
+  }
 
 
   return (
@@ -78,7 +106,7 @@ function Cards({ artist, photo, location, date, likes, posts, setPosts, post }) 
           </Card.Body> */}
           <Row>
             <Col className="p-1 col-4">
-            <button className="btn btn-outline-danger w-25 d-flex gap-2 p-1 ">
+            <button onClick={handleLikes} className="btn btn-outline-danger w-25 d-flex gap-2 p-1 ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
