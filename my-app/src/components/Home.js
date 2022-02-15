38d1b0499cd8fc4c1445e439d4c717f3728931ba
Haleem Hamid <hhamidbiz@gmail.com>
2022-02-15 12:55:33 -0800

    Modal set up/ Need to connect post

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -7,16 +7,11 @@ function Home() {
   return (
     <div>
       <Container>
-        <NewPost />
-        
         <div className="container-fluid bg-light text-dark p-5">
           <div className="container bg-light p-5">
             <h1 className="display-4 fw-bold">Welcome to Admin Dashboard</h1>
             <hr />
-            <p>Go to My Website</p>
-            <a href="#" className="btn btn-primary">
-              link
-            </a>
+            <NewPost />
           </div>
           <CardList />
         </div>
--- my-app/src/components/NewPost.js
+++ my-app/src/components/NewPost.js
@@ -1,64 +1,37 @@
-import React from 'react'
-import { Form, Button, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button, Row, Col, Modal } from "react-bootstrap";
 
 export default function NewPost() {
+  const [show, setShow] = useState(false);
+  const [newPost, setNewPost] = useState({});
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  function handleSubmit(e) {
+    handleClose();
+  }
+
   return (
     <div>
-      <Col>
-        <div className="pt-5 mt-5 mb-5 pb-5 w-50 mx-auto align-center">
-          <Form>
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-            </Row>
-
-            <Form.Group className="mb-3" controlId="formGridAddress1">
-              <Form.Label>Address</Form.Label>
-              <Form.Control placeholder="1234 Main St" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formGridAddress2">
-              <Form.Label>Address 2</Form.Label>
-              <Form.Control placeholder="Apartment, studio, or floor" />
-            </Form.Group>
-
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>City</Form.Label>
-                <Form.Control />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridState">
-                <Form.Label>State</Form.Label>
-                <Form.Select defaultValue="Choose...">
-                  <option>Choose...</option>
-                  <option>...</option>
-                </Form.Select>
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridZip">
-                <Form.Label>Zip</Form.Label>
-                <Form.Control />
-              </Form.Group>
-            </Row>
-
-            <Form.Group className="mb-3" id="formGridCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </div>
-      </Col>
+      <Button variant="primary" onClick={handleShow}>
+        {" "}
+        Create Post{" "}
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Post New Art</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Control size="sm" type="text" placeholder="Img URL" />
+          <Form.Control size="sm" type="text" placeholder="Location" />
+          <Form.Control size="sm" type="text" placeholder="Artist" />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
