ee9447483770435b9bed1f49cc01a2beb8200ecd
Myles <bracken.myles@gmail.com>
2022-02-15 23:48:12 -0800

    Header styling changes

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -19,18 +19,31 @@ function Home() {
   return (
     <div>
       <Container fluid className="bg-light bkgrnd-img">
-        <Col fluid className="container-fluid bg-light text-dark shadow-lg">
+        <Col fluid className="container-fluid bg-light text-dark shadow-lg border-1">
           <Col className="container p-5">
             <div className="text-center">
-              <img src="./urban_walls_logo.png" className="rounded w-50" alt="Urban Walls Logo" />
+              <img
+                src="./urban_walls_logo.png"
+                className="rounded w-50"
+                alt="Urban Walls Logo"
+              />
             </div>
-            <p className="w-50 mx-auto">Welcome to Urban Walls, a community helping showcase artwork, murals, and graffiti found in various urban settings.</p>
-            <hr />
-            <Col><h5>Create a Post or View the Gallery Below</h5></Col>
+            <p className="w-50 mx-auto mt-3 mb-3">
+              Welcome to Urban Walls, a community helping showcase artwork,
+              murals, and graffiti found in various urban settings.
+            </p>
+            <hr className="w-75 mx-auto mt-5 mb-5 bg-secondary" />
+            <Col>
+              <h5 className="w-50 mx-auto mt-5">
+                Create a Post or View the Gallery Below
+              </h5>
+            </Col>
+            <Col className="mx-auto mt-3 mb-5">
             <NewPost setNewPost={setNewPost} />
+            </Col>
           </Col>
         </Col>
-          
+
         <Col>
           <CardList posts={posts} setPosts={setPosts} />
         </Col>
